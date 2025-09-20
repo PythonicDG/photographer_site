0e1ccfb9b41e00404dee14f18beacf61ec55e1f7
PythonicDG <dipakgaikwadmg@gmail.com>
2025-09-20 17:31:19 +0530

    [fix] static files issues

--- photographer_site/urls.py
+++ photographer_site/urls.py
@@ -4,8 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin', admin.site.urls),
-
+    path('admin/', admin.site.urls),
     path('', include('core.urls')),
     path('portfolio/', include('portfolio.urls')),
     path('about/', include('about.urls')),
@@ -15,3 +14,4 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
