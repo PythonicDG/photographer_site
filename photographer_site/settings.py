1dfed70ee99df0b762313dde1fe2d63e267e420c
PythonicDG <dipakgaikwadmg@gmail.com>
2025-09-20 17:25:44 +0530

    [fix] static files issues

--- photographer_site/settings.py
+++ photographer_site/settings.py
@@ -90,6 +90,14 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
+
+
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOW_ALL_ORIGINS = config('CORS_ALLOW_ALL_ORIGINS', default=True, cast=bool)
